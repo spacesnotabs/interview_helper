77dde1838d6e7f84c0a17b01b4457974121b93c0
spacesnotabs <127076306+spacesnotabs@users.noreply.github.com>
2025-03-25 10:45:25 -0700

    Update app.py
    Change host for Render

--- app.py
+++ app.py
@@ -207,5 +207,4 @@ if __name__ == '__main__':
     
     # No need to pre-generate challenges anymore
     # The app will generate fresh challenges on demand
-    
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
